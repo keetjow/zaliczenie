e036458b9867c5c13d957fdf7a724a8fcba33e56
keetjow <114618649+keetjow@users.noreply.github.com>
2022-12-10 18:30:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,182 +1,54 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <math.h>
 #include <vector>
-#include <cstdlib>
-#include <time.h>
-
-class Bullet
-{
-public:
-    sf::Texture bulletTexture;
-    sf::Sprite bulletBody;
-
-    sf::Vector2f velocity;
-    float speed = 10.f;
-
-    Bullet(sf::Vector2f velocity, sf::Vector2f position)
-    {
-        this->bulletTexture.loadFromFile("spr_orange.png");
-        this->bulletBody.setTexture(this->bulletTexture);
-        this->bulletBody.setOrigin(8.f, 8.f);
-        this->velocity = velocity;
-        this->bulletBody.setPosition(position);
-    }
-};
-
-class Enemy
-{
-public:
-    sf::Texture enemyTexture;
-    sf::Sprite enemyBody;
-
-    float speed = 2.f;
-
-    sf::Vector2f velocity(sf::Vector2f playerPos)
-    {
-        sf::Vector2f vel = playerPos - sf::Vector2f(this->enemyBody.getPosition().x, this->enemyBody.getPosition().y);
-        float dif = std::sqrt(std::pow(vel.x, 2) + std::pow(vel.y, 2));
-        return vel/dif;
-    }
-
-    Enemy()
-    {
-        int randomnumber = rand()%3;
-        switch(randomnumber)
-        {
-        case 0:
-            {
-                this->enemyTexture.loadFromFile("spr_ghost_hard.png");
-                break;
-            }
-        case 1:
-            {
-                this->enemyTexture.loadFromFile("spr_ghost.png");
-                break;
-            }
-        case 2:
-            {
-                this->enemyTexture.loadFromFile("spr_blob.png");
-                break;
-            }
-        }
-        this->enemyBody.setTexture(enemyTexture);
-        this->enemyBody.setTextureRect(sf::IntRect(0, 0, 16, 16));
-        this->enemyBody.setOrigin(this->enemyBody.getPosition().x + 8,this->enemyBody.getPosition().y + 8);
-        this->enemyBody.setPosition(rand()%1280, rand()%720);
-        this->enemyBody.setScale(2.f, 2.f);
-    }
-};
-
-
-
-
-
+#include "classes.h"
 
 int main()
 {
-    srand(time(NULL));
+    //Window setup
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8.f;
+    sf::RenderWindow gameWindow(sf::VideoMode(1280, 720, 32), "Projekt na zaliczenie", sf::Style::Fullscreen, settings);
+    gameWindow.setFramerateLimit(60);
+    gameWindow.setVerticalSyncEnabled(true);
 
-    sf::RenderWindow okno(sf::VideoMode(1280, 720, 32), "Testowa gra", sf::Style::Fullscreen, settings);
-    okno.setFramerateLimit(60);
-    okno.setVerticalSyncEnabled(true);
+    //Projectiles setup
+    std::vector<Projectile*> projectiles;
 
-    sf::Texture playerTexture;
-    playerTexture.loadFromFile("player.png");
-    sf::IntRect playerAnim(0, 0, 16, 16);
+    //Player setup
+    sf::Vector2f mousePos;
+    Player player(gameWindow, mousePos, projectiles);
 
-
-    sf::Sprite player;
-    player.setTexture(playerTexture);
-    player.setTextureRect(playerAnim);
-    player.setOrigin(player.getPosition().x + 8, player.getPosition().y + 8);
-    player.setPosition(okno.getSize().x/2, okno.getSize().y/2);
-    player.scale(2, 2);
-    float playerSpeed = 5.f;
-    float fireRate = 100;
-
-    sf::Clock shootClock;
-    sf::Clock enemySpawnClock;
-
-    std::vector<Enemy*> enemies;
-    float enemySpawnRate = 1;
-
-    std::vector<Bullet*> bullets;
-
-    sf::Event event;
-    while(okno.isOpen())
+    //Main loop
+    while(gameWindow.isOpen())
     {
-        while(okno.pollEvent(event))
+        //Event loop
+        sf::Event event;
+        while(gameWindow.pollEvent(event))
         {
             if(event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                okno.close();
-        }
-        sf::Vector2f playerPos = sf::Vector2f(player.getPosition());
-        sf::Vector2f mousePos = sf::Vector2f(sf::Mouse::getPosition(okno));
-        sf::Vector2f velocity = mousePos - playerPos;
-        float velocityLength = std::sqrt(std::pow(velocity.x, 2) + std::pow(velocity.y, 2));
-
-        float playerAngle = std::atan2(velocity.x, velocity.y) * 180/M_PI;
-        player.setRotation(-playerAngle);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            player.move(0.f, -1.f*playerSpeed);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-            player.move(0.f, 1.f*playerSpeed);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            player.move(-1.f*playerSpeed, 0.f);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            player.move(1.f*playerSpeed, 0.f);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-            fireRate = 50;
-        else
-            fireRate = 200;
-
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && shootClock.getElapsedTime().asMilliseconds() > fireRate)
-        {
-            bullets.push_back(new Bullet(velocity/velocityLength, playerPos));
-            shootClock.restart();
+                gameWindow.close();
         }
 
-        if(enemySpawnClock.getElapsedTime().asSeconds() > 1)
-        {
-            enemies.push_back(new Enemy);
-            enemySpawnClock.restart();
-        }
+        //Update mouse position
+        mousePos = sf::Vector2f(sf::Mouse::getPosition(gameWindow));
 
-        for(int i = 0; i < bullets.size(); i++)
-        {
-            bullets[i]->bulletBody.move(bullets[i]->velocity * bullets[i]->speed);
-        }
+        //Game logic
+        player.update();
 
-        for(int i = 0; i < enemies.size(); i++)
-        {
-            enemies[i]->enemyBody.move(enemies[i]->velocity(playerPos) * enemies[i]->speed);
-        }
+        for(int i = 0; i < projectiles.size(); i++)
+            projectiles[i]->update();
 
+        //Drawing sprites
+        gameWindow.clear();
 
-        okno.clear(sf::Color::Black);
+        player.draw();
 
-        for(int i = 0; i < bullets.size(); i++)
-        {
-            okno.draw(bullets[i]->bulletBody);
-        }
+        for(int i = 0; i < projectiles.size(); i++)
+            projectiles[i]->draw();
 
-        for(int i = 0; i < enemies.size(); i++)
-        {
-            okno.draw(enemies[i]->enemyBody);
-        }
-
-        okno.draw(player);
-        okno.display();
+        //Final frame display
+        gameWindow.display();
     }
-
-
     return 0;
 }
