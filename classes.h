2da68b487b4c1ffabbb38871fa975c7dee5d6cad
keetjow <114618649+keetjow@users.noreply.github.com>
2022-12-14 17:42:33 +0100

    Update classes.h

--- classes.h
+++ classes.h
@@ -6,12 +6,13 @@ class Projectile
 {
     public:
         float speed = 1000;
+        float damage;
         sf::Texture texture;
         sf::Sprite body;
         sf::RenderWindow &window;
         sf::Vector2f velocity;
         sf::Vector2f velocityN;
-        Projectile(sf::RenderWindow &windowRef, sf::Vector2f startPos, sf::Vector2f velocity);
+        Projectile(sf::RenderWindow &windowRef, sf::Vector2f startPos, sf::Vector2f velocity, float damageAmount);
         void update(float &dt);
         void draw();
         //debug
@@ -21,17 +22,18 @@ class Projectile
 class Entity
 {
     protected:
-        float hp;
-        float movementSpeed;
-        float attackPower;
         sf::RenderWindow &window;
         sf::Texture texture;
 
     public:
+        float hp;
+        float movementSpeed;
+        float attackPower;
         sf::Sprite body;
         void draw();
         virtual void update(float &dt) = 0;
         Entity(sf::RenderWindow &windowRefBase);
+        bool alive = true;
 };
 
 class Player : public Entity
@@ -41,9 +43,11 @@ class Player : public Entity
         sf::Vector2f &mousePos;
         std::vector<Projectile*> &projectiles;
         sf::Clock shootTimer;
+        sf::Clock abilityCooldown;
         Player(sf::RenderWindow &windowRef, sf::Vector2f &mousePosRef, std::vector<Projectile*> &projectilesRef);
         void update(float &dt);
         void shoot();
+        void useAbility();
 };
 
 class Enemy : public Entity
@@ -51,8 +55,13 @@ class Enemy : public Entity
     public:
         Entity* target;
         void update(float &dt);
+        bool knockbackState = false;
+        sf::Clock knockbackTimer;
         Enemy(sf::RenderWindow &windowRef, Entity* targetRef);
         sf::Vector2f velocity;
+        sf::Vector2f moveVector;
+        void takeDamage(float damageReceiveAmount);
+        void punch(Entity* punchTarget);
         //debug
         //~Enemy();
 };
@@ -74,3 +83,4 @@ class Slime : public Enemy
     public:
     Slime(sf::RenderWindow &windowRef, Entity* targetRef, sf::Vector2f startPos);
 };
+
